bd912d7491f0593a378a549c85f8a10c4c67621b
makakochka <makakochka@gmail.com>
2024-12-10 17:08:23 +0900

    second commit

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import time
 
 r = requests.get("https://hacker-news.firebaseio.com/v0/topstories.json")
 ids = r.json()
+# print(type(ids))
 
 top30_stories = []
 for id in ids[:30]:
@@ -12,7 +13,5 @@ for id in ids[:30]:
     response = requests.get(url)
     data = response.json()
     top30_stories.append({"title": data.get("title", "No Title"), "url": data.get("url", "No URL")})
-
-for story in top30_stories:
     time.sleep(1)
-    print("{" + f"Title: {story['title']} URL: {story['url']}" + "}")
+    print("{" + f"Title: {data['title']} URL: {data['url']}" + "}")
